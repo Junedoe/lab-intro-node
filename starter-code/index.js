6ec69a269cc5874db8e7b7faae4ef4185459541d
Ojuna Faust <ojunafaust@MacbookHotspot.local>
2018-08-22 11:02:40 +0200

    BER -Ojuna - done yesterday, forgot to commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,32 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
+    add(x) {
+        this.items.push(x);
+        this.length++;
+        this.items.sort((a, b) => {
+            return a - b;
+        });
+    }
+    get(pos) {
+        return this.items[pos - 1];
+    }
+    max() {
+        return Math.max(...this.items);
+    }
+    min() {
+        return Math.min(...this.items);
+    }
+    average() {
+        return this.sum() / this.length;
+    }
+    sum() {
+        return this.items.reduce(function(acc, currentVal) {
+            return acc + currentVal;
+        }, 0);
+    }
+}
 
 module.exports = SortedList;
